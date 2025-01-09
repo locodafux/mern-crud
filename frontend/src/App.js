9be3af07c9cbc9e773dfd1645b58afb2d54d6c32
leonardo <locodafux@gmail.com>
2025-01-09 20:54:38 +0800

    trying out frontend of react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+
+
+function MyButton() {
+  return (
+    <button>Click Me</button>
+  )
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class = "screen">
+      <h1>Welcome to my app</h1>
+      <MyButton />
     </div>
   );
 }
