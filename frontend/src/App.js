0824ba29ca89ba6768baa3cc9ce9e77821651f44
leonardo <locodafux@gmail.com>
2025-01-12 21:41:40 +0800

    added sample navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Home from './Pages/Home.js';
+import Navbar from './Components/Navbar.jsx';
 
 export default function App() {
   return (
       <div class = "screen">
+      <Navbar />
       <Home />
     </div>
   );
