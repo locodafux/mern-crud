d1b8d90ec7962336a930a46c28f00b51b19613dd
leonardo <locodafux@gmail.com>
2025-01-22 21:04:06 +0800

    backend getusers sample

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from 'express'; 
+var app = express();
+import { readFile } from 'fs'; 
+import path from 'path';  
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+// Endpoint to Get a list of users
+app.get('/getUsers', function(req, res){
+    readFile(__dirname + "/" + "users.json", 'utf8', function(err, data){
+        console.log(data);
+        res.end(data); 
+    });
+})
+
+// Create a server to listen at port 8080
+const server = app.listen(8080, '127.0.0.1', function(){
+    const host = server.address().address;
+    const port = server.address().port;
+    console.log(`REST API demo app listening at http://${host}:${port}`);
+});
