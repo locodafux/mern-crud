16f2921255056c9ba78a4b06a8dc158be63c45b1
locodafux <locodafux@gmail.com>
2025-02-11 20:04:05 +0800

    cleaned users

--- backend/routes/User.js
+++ backend/routes/User.js
@@ -3,39 +3,6 @@ import { uploadProcessedData } from '../lib/firebase.js';
 
 const router = express.Router(); 
 
-router.get('/friends/:name', (req, res) => {
-    const { name } = req.params;
-    if (!name || !(name in friends)) {
-        return res.sendStatus(400);
-    }
-    res.status(200).send({ name: friends[name] });
-});
 
-router.post('/addfriend', (req, res) => {
-    const { name, status } = req.body;
-    if (!name || !status) {
-        return res.sendStatus(400); 
-    }
-    friends[name] = status;
-    res.status(200).send(friends);
-});
-
-router.patch('/changestatus', (req, res) => {
-    const { name, newStatus } = req.body;
-    if (!name || !newStatus || !(name in friends)) {
-        return res.sendStatus(400); 
-    }
-    friends[name] = newStatus;
-    res.status(200).send(friends);
-});
-
-router.delete('/friends', (req, res) => {
-    const { name } = req.body;
-    if (!name || !(name in friends)) {
-        return res.sendStatus(400); 
-    }
-    delete friends[name];
-    res.status(200).send(friends);
-});
 
 export default router;
