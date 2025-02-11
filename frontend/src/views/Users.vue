1cfde8b1c88e2a0c82d6a869b8464decc47c31ec
locodafux <locodafux@gmail.com>
2025-02-11 20:16:22 +0800

    Fixed user frontend

--- frontend/src/views/Users.vue
+++ frontend/src/views/Users.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="max-w-2xl mx-auto p-6 bg-white shadow-md rounded-lg">
-    <h2 class="text-xl font-semibold text-gray-800 mb-4">Add New Role</h2>
+    <h2 class="text-xl font-semibold text-gray-800 mb-4">Add New User</h2>
 
     <div class="space-y-4">
-      <input type="text" v-model="user.fullName" placeholder="Role Name"
+      <input type="text" v-model="user.username" placeholder="Username"
+        class="w-full px-4 py-2 border rounded-lg bg-gray-100" />
+      <input type="text" v-model="user.password" placeholder="Password"
+        class="w-full px-4 py-2 border rounded-lg bg-gray-100" />
+      <input type="text" v-model="user.fullName" placeholder="Full Name"
         class="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none" />
-      <input type="text" v-model="user.role" placeholder="Slug" disabled
-        class="w-full px-4 py-2 border rounded-lg bg-gray-100" />
-      <input type="text" v-model="user.username" placeholder="Slug" disabled
-        class="w-full px-4 py-2 border rounded-lg bg-gray-100" />
-      <input type="text" v-model="user.password" placeholder="Slug" disabled
+      <input type="text" v-model="user.role" placeholder="Role"
         class="w-full px-4 py-2 border rounded-lg bg-gray-100" />
       <button @click="addRole" class="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition">
         Add Role
@@ -19,6 +19,15 @@
 </template>
 <script>
 export default {
-
+  data() {
+    return {
+      user: {
+        fullName: null,
+        role: null,
+        username: null,
+        password: null,
+      }
+    }
+  }
 }
 </script>
